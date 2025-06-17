b2f026236bb35502660bb6b70bf0afdfd96f90ba
Andrew Noonan <anoon@AndrewNoonan.localdomain>
2025-06-17 00:07:05 -0500

    Try agaiin

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react-swc'
 import path from 'path'
 
 export default defineConfig({
-  base: 'https://github.com/andrew-noonan/website',
+  base: '/website/',
   plugins: [react()],
   resolve: {
     alias: {
